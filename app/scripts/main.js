1cf679d2c0397e513b0d2794a3279a2521074923
saikatharryc <saikatchakrabortty2@gmail.com>
2017-04-02 02:40:57 +0530

    fix everything

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+$(function(){
+		$(".cover-heading").typed({
+			strings: ["Hiii", "Nice to meet you!!", "Let me introduce my self...", "I'm Saikat...", "   Saikat Chakrabortty   ", "i do developments." , "And...", "I Really Like the simple things..", ">_   "],
+			typeSpeed: 150,
+			loop: false
+		});
+		
+	});
